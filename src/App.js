4e70fb567102439eaa23397eced6c406488e2ce5
Clashix <84671657+Clashix@users.noreply.github.com>
2022-06-14 00:45:11 +0200

    BUG CONSOLE WIDTH

--- src/App.js
+++ src/App.js
@@ -2,7 +2,23 @@ import './App.css';
 import Header from "./components/header.js";
 import React, { useState, useEffect } from "react";
 
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return width;
+}
+
 function App() {
+  const [width, setWidth] = useState(getWindowDimensions());
+
+  useEffect(() => {
+    window.addEventListener("resize", () => setWidth(getWindowDimensions()));
+    return () =>
+      window.removeEventListener("resize", () =>
+        setWidth(getWindowDimensions())
+      );
+  });
+
+  useEffect(() => console.log(width));
   return (
     <div className="App">
       <Header />
--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../style/header.css";
 import logo from "../assets/isepToken.png";
 
+
 function Header() {
+    const [toggleMenu, setToggleMenu] = useState(false);
+    
+    const [largeur, setLargeur] = useState(window.innerWidth);
+
+    const toggleNavSmallScreen = () => {
+      setToggleMenu(!toggleMenu);
+    }
+    
+
+useEffect (() => {
+
+    const changeWidth = () => {
+        setLargeur(window.innerWidth);
+
+        if(window.innerWidth > 500){
+            setToggleMenu(false);
+        }
+    }
+
+    window.addEventListener('resize', changeWidth);
+
+    return () => {
+        window.removeEventListener('resize', changeWidth);
+    }
+}
+)
+
   return (
     <div className="header">
       <div>
@@ -14,13 +42,16 @@ function Header() {
             className="logo_image"
           />
       </div>
-      <div className="navBar">
-        <a href="#about">About</a>
-        <a href="#ourgoal">Our goal</a>
-        <a href="#whitepaper">Whitepaper</a>
-        <a href="#roadmap">Roadmap</a>
-        <a href="#contact">Contact</a>
-      </div>
+      {(toggleMenu || largeur > 500) && (
+        <div className="navBar">
+            <a href="#about">About</a>
+            <a href="#ourgoal">Our goal</a>
+            <a href="#whitepaper">Whitepaper</a>
+            <a href="#roadmap">Roadmap</a>
+            <a href="#contact">Contact</a>
+        </div>
+      )}
+      <button onClick={toggleNavSmallScreen} className="btn-header">CQFD</button>
     </div>
   );
 }
