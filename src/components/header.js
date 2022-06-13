0074c519a2dc929d0700eed741dbf219aed780fc
Clashix <84671657+Clashix@users.noreply.github.com>
2022-06-13 18:44:26 +0200

    debut

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React from "react";
+import "../style/header.css";
+import logo from "../assets/isepToken.png";
 
 function Header() {
-    return (
-        <div className='header'>
-            <div className='headerContainer'>
-                <h1>Bonjour</h1>
-            </div>
-        </div>
-    );
-};
+  return (
+    <nav className="header">
+      <a href="">
+        <img
+          src={logo}
+          width="75"
+          height="75"
+          alt="Logo"
+          className="logo_image"
+        />
+      </a>
+      <div className="navBar">
+        <a href="#about">About</a>
+        <a href="#ourgoal">Our goal</a>
+        <a href="#whitepaper">Whitepaper</a>
+        <a href="#roadmap">Roadmap</a>
+        <a href="#contact">Contact</a>
+      </div>
+    </nav>
+  );
+}
 
 export default Header;
