ad3e86a2b2c62da8ca32f60ecb1d32acbd0bb617
Clashix <84671657+Clashix@users.noreply.github.com>
2022-06-15 19:09:17 +0200

    clean manque css

--- src/components/header.js
+++ src/components/header.js
@@ -10,12 +10,19 @@ function getWindowDimensions() {
 function Header() {
   const [width, setWidth] = useState(getWindowDimensions());
 
+  const [isOpen, setOpen] = useState(false);
+
   useEffect(() => {
     window.addEventListener("resize", () => setWidth(getWindowDimensions()));
     return () =>
     window.removeEventListener("resize", () => setWidth(getWindowDimensions()));
   }
   )
+
+  function btnFunction() {
+    console.log(isOpen);
+    setOpen(!isOpen);
+  }
   return (
     <div className="header">
       <div>
@@ -27,14 +34,24 @@ function Header() {
             className="logo_image"
           />
       </div>
-        {width > 500 && <div className="navBar">
+        {width > 500 && 
+        <div className="navBar">
             <a className="items-navBar" href="#about">About</a>
             <a className="items-navBar" href="#ourgoal">Our goal</a>
             <a className="items-navBar" href="#whitepaper">Whitepaper</a>
             <a className="items-navBar" href="#roadmap">Roadmap</a>
             <a className="items-navBar" href="#contact">Contact</a>
         </div>}
-      {width < 500 && <button className="btn-header">CQFD</button>}
+      {width < 500 && <button onClick={() => btnFunction() } className="btn-header">CQFD</button>}
+      {isOpen && (
+        <div className="navBar-open">
+          <a className="items-navBar-open" href="#about">About</a>
+          <a className="items-navBar-open" href="#ourgoal">Our goal</a>
+          <a className="items-navBar-open" href="#whitepaper">Whitepaper</a>
+          <a className="items-navBar-open" href="#roadmap">Roadmap</a>
+          <a className="items-navBar-open" href="#contact">Contact</a>
+        </div>)
+      }
     </div>
   );
 }
