ff0abb5c86302743f6d64fc315194498bd59ea6b
Clashix <84671657+Clashix@users.noreply.github.com>
2022-06-22 11:31:04 +0200

    text

--- src/components/performance.js
+++ src/components/performance.js
@@ -4,8 +4,12 @@ import "../style/performance.css";
 function Performance() {
   return (
     <div className="performance">
-      <p className="titlePerformance"></p>
-      <p className="textPerformance"></p>
+      <p className="titlePerformance">Our performance</p>
+      <p className="textPerformance">
+        Our past performances don’t reflect our future performances. <br />
+        We never know how the market can go, investing may leads to loss of
+        money !
+      </p>
     </div>
   );
 }
