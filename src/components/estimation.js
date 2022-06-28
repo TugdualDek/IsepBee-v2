b93fd232d528de5d40c29afb19545ff2c4036fdd
Clashix <84671657+Clashix@users.noreply.github.com>
2022-06-28 19:03:40 +0200

    estimation final

--- src/components/estimation.js
+++ src/components/estimation.js
@@ -14,24 +14,26 @@ function Estimation() {
         estimation because we may invest in DEFI, <br /> in this case it is just
         an estimation.
       </p>
-      <button className="buttonWallet" onClick={togglePopup}>
-        See more
-      </button>
-      {isOpen && (
-        <Popup
-          content={
-            <>
-              <b>Design your Popup</b>
-              <p style={{ color: "black" }}>
-                Our past performances don’t reflect our future performances. We
-                never know how the market can go, investing may leads to loss of
-                money
-              </p>
-            </>
-          }
-          handleClose={togglePopup}
-        />
-      )}
+      <div className="buttonEstimation">
+        <button className="buttonWallet" onClick={togglePopup}>
+          See more
+        </button>
+        {isOpen && (
+          <Popup
+            content={
+              <>
+                <b>Design your Popup</b>
+                <p style={{ color: "black" }}>
+                  Our past performances don’t reflect our future performances.
+                  We never know how the market can go, investing may leads to
+                  loss of money
+                </p>
+              </>
+            }
+            handleClose={togglePopup}
+          />
+        )}
+      </div>
     </div>
   );
 }
