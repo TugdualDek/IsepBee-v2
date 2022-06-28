c40b26ab090b0391347b80e56526bc94723adc52
Clashix <84671657+Clashix@users.noreply.github.com>
2022-06-28 12:04:55 +0200

    chart

--- src/components/chart.js
+++ src/components/chart.js
@@ -148,13 +148,19 @@ const data = [
 
 export default function App() {
   return (
-    <div style={{ width: "100%", height: 300 }}>
+    <div
+      style={{
+        width: "80%",
+        height: 300,
+        marginLeft: "7%",
+      }}
+    >
       <ResponsiveContainer>
         <AreaChart
           data={data}
           margin={{
             top: 10,
-            right: 30,
+            right: 0,
             left: 0,
             bottom: 0,
           }}
