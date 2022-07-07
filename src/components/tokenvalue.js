c3b8c8041f3b41f952b4478485ba5f18cda6641b
Clashix <84671657+Clashix@users.noreply.github.com>
2022-07-07 16:15:33 +0200

    tokenvalue

---
+++ src/components/tokenvalue.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Tokenvalue() {
+    return (
+        <div>
+            <p className="tokenTitle">Token value</p>
+            <p className="tokenText">
+                That’s our wallet value. <br /> It is based on the total value and the
+                total supply.
+            </p>
+        </div>
+    );
+};
+
+export default Tokenvalue;
--- src/components/wallet.js
+++ src/components/wallet.js
@@ -3,6 +3,7 @@ import Piechart from "./piechart.js";
 import Estimation from "./estimation.js";
 import Chart from "./chartstatistique.js";
 import Statistique from "./statistique.js";
+import Tokenvalue from "./tokenvalue.js";
 import "../style/wallet.css";
 
 function Wallet() {
@@ -17,6 +18,9 @@ function Wallet() {
         <div className="walletComponent2">
           <Chart />
           <Statistique />
+        </div>
+        <div className="walletComponent3">
+          <Tokenvalue />
         </div>
       </div>
     </div>
